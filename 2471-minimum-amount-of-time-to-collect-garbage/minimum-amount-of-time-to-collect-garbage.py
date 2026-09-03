04aac6aad6b0421306f96be8c8b3b104b4890876
Daniel K. Ayivon <84874876+kdayivon@users.noreply.github.com>
2026-09-03 15:13:18 +0200

    Time: 195 ms (22.63%) | Memory: 37.3 MB (35.52%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.py
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.py
@@ -3,22 +3,19 @@ class Solution:
         # we have to tell the trucks the "addresses" of where to pick up the garbage
         total = 0
         address = {}
-        preSum = [0]
+        pickup = {}
         truck = "MPG"
-
-        for i, house in enumerate(garbage):
-            address[i] = list(house)
+        preSum = [0]
 
         for i in range(len(travel)):
             preSum.append(preSum[-1] + travel[i])
 
-        for i in range(len(truck)):
-            pickup,  timing = 0, 0
-            for key, value in address.items():
-                if truck[i] in value:
-                    timing = preSum[key]
-                    pickup += value.count(truck[i])
-            print(timing, pickup)
-            print("---")
-            total += timing + pickup
+        for i in range(len(garbage)):
+            for c in garbage[i]:
+                address[c] = i
+                pickup[c] = pickup.get(c, 0) + 1
+        for t in truck:
+            if t in pickup:
+                total += preSum[address[t]] + pickup[t]
         return total
+       
