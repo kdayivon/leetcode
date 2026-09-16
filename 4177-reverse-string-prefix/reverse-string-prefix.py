dae3bb39415e8c3fcc530ba41938fa2c197cdb98
Daniel K. Ayivon <84874876+kdayivon@users.noreply.github.com>
2026-09-16 15:21:41 +0200

    Time: 0 ms (100.00%) | Memory: 19.2 MB (52.22%) - LeetSync

--- 4177-reverse-string-prefix/reverse-string-prefix.py
+++ 4177-reverse-string-prefix/reverse-string-prefix.py
@@ -2,13 +2,9 @@ class Solution:
     def reversePrefix(self, s: str, k: int) -> str:
         stringList = list(s)
         i = k-1
-        for j in range(k-1):
-            if i == j:
-                return "".join(stringList)
+        for j in range(k // 2):
             temp = stringList[j]
             stringList[j] = stringList[i]
             stringList[i] = temp
             i -= 1
-            if i == j:
-                return "".join(stringList)
         return "".join(stringList)
